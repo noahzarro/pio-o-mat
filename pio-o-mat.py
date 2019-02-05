3d5ee6a617b2d747e076a6bf54cdb8b4fdea3097
zarron <zarron@ethz.ch>
2019-02-05 17:54:54 +0100

    welcome screen

--- pio-o-mat.py
+++ pio-o-mat.py
@@ -17,3 +17,27 @@ from PIL import ImageFont
 import SimpleMFRC522
 
 import piorist.py
+
+# setup RFID-Device
+card_reader = SimpleMFRC522.SimpleMFRC522()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = 128
+height = 64
+image = Image.new('1', (width, height))
+
+serial = spi(device=0, port=0, bus_speed_hz=8000000, transfer_size=4096, gpio_DC=24, gpio_RST=25)
+
+device = sh1106(serial, rotate=2)  # sh1106
+
+try:
+    with canvas(device) as draw:
+        # draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((0, 0), "Willkommen zum Pio-o-Mat", fill="white")
+        draw.text((0, 8), "Drücke OK fürs Menu", fill="white")
+        draw.text((0, 16), "Drücke Pio für Pio", fill="white")
+except:
+    print("except")
+
+GPIO.cleanup()
