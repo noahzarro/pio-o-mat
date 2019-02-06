8026dc7c9e1115580f9c908bf44d934f99754cee
zarron <zarron@ethz.ch>
2019-02-06 15:15:39 +0100

    RFID-Reader Test3

--- RFID-Read.py
+++ RFID-Read.py
@@ -1,4 +1,5 @@
 import SimpleMFRC522
+import time
 
 myReader = SimpleMFRC522.SimpleMFRC522()
 r=myReader.read()
@@ -6,3 +7,11 @@ print("hello")
 print(type(r[1]))
 print(r[1])
 
+myReader.write("hello")
+
+time.sleep(3)
+
+r=myReader.read()
+print("hello")
+print(type(r[1]))
+print(r[1])
