4aace2c486cb7dcbf384cd0c7a633f5c23cbb421
zarron <zarron@ethz.ch>
2019-02-06 16:41:46 +0100

    add piorist implemented2 debug8

--- RFID-Write.py
+++ RFID-Write.py
@@ -1,4 +1,5 @@
 import SimpleMFRC522
+import json
 
 myReader = SimpleMFRC522.SimpleMFRC522()
 r=myReader.read()
@@ -6,3 +7,5 @@ r=myReader.read()
 print(r[1])
 
 myReader.write("")
+with open("test","w") as list_file:
+    json.dump(r[1],list_file)
