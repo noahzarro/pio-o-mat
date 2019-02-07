d7b658e3488434c08fcb52c1a1587f629325d507
zarron <zarron@ethz.ch>
2019-02-07 11:29:50 +0100

    new connection added3

--- pio-o-mat.py
+++ pio-o-mat.py
@@ -563,7 +563,7 @@ def new_connection():
             break
 
     with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as file_write:
-        file_write.write("\r\nnetwork={\r\n   ssid=" + connection_data["ssid"] + "\r\n   psk=" + connection_data["passwort"] + "\r\n}")
+        file_write.write("\r\nnetwork={\r\n   ssid=\"" + connection_data["ssid"] + "\"\r\n   psk=\"" + connection_data["passwort"] + "\"\r\n}")
 
     with canvas(device) as draw:
         display_title("Neue Verbindung", draw)
