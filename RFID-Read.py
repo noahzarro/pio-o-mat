5245a8799a09b008dc2f85461cd3583b9576fd72
zarron <zarron@student.ethz.ch>
2019-06-13 15:39:25 +0200

    decode

--- RFID-Read.py
+++ RFID-Read.py
@@ -4,14 +4,14 @@ myReader = SimpleMFRC522.SimpleMFRC522()
 r=myReader.read()
 
 print("Pio Card:")
-print(r[1])
+print(r[1].decode('latin_1'))
 print("length: "+ str(len(r[1])))
 print(type(r[1]))
 
 r=myReader.read_swiss_pass()
 
 print("Swiss Pass Card:")
-print(r[1])
+print(r[1].decode('latin_1'))
 print("length: "+ str(len(r[1])))
 print(type(r[1]))
 
@@ -20,7 +20,7 @@ id = None
 while id == None:
     id, text = myReader.read_no_block_middle()
 
-print("Swiss Pass Card:")
-print(text)
+print("Swiss Middle:")
+print(text.decode('latin_1'))
 print("length: "+ str(text))
 print(type(text))
